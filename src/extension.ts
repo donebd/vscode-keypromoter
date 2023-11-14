a8c6cc016d14fd906faedaf2a9e6d9cc5f014cff
dbakin <dbakin@ptsecurity.com>
2023-11-14 15:50:26 +0300

    SubscriptionService

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { uIOhook } from 'uiohook-napi'
+import { SubscriptionService } from './services/subscriptionService';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,6 +14,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-keypromoter" is now active!');
+
+	const subscriptionService = new SubscriptionService();
+	subscriptionService.listenForPossibleShortcutActions();
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
---
+++ src/services/subscriptionService.ts
@@ -0,0 +1,107 @@
+import { Subject } from 'rxjs';
+import * as vscode from 'vscode';
+
+export class SubscriptionService {
+
+    private readonly pipe = new Subject<[string, ...any]>();
+    private readonly commandIdToOverloadHandlerMap: Map<string, vscode.Disposable> = new Map();
+
+    public async listenForPossibleShortcutActions() {
+        const commandIds = await vscode.commands.getCommands(true);
+        this.listenVscodeCommands(commandIds);
+        this.listenPublicVscodeApi();
+    }
+
+    private listenVscodeCommands(commandIds: string[]) {
+        const commandHandler = async (commandId: string, args: any) => {
+            this.commandIdToOverloadHandlerMap.get(commandId)!.dispose();
+            this.pipe.next([commandId, args]);
+        };
+
+        commandIds.forEach(commandId => {
+            try {
+                const overloadedHandler = vscode.commands.registerCommand(commandId, async (args) => commandHandler(commandId, args));
+                this.commandIdToOverloadHandlerMap.set(
+                    commandId,
+                    overloadedHandler
+                );
+            } catch (e) {
+                console.log(`${commandId} can't be overloaded`)
+            }
+        });
+
+        this.pipe.subscribe(async (next) => {
+            const commandId = next[0];
+            console.log(commandId + " executed!");
+            vscode.window.showInformationMessage(next[0] + " executed!");
+            if (next[1]) {
+                await vscode.commands.executeCommand(commandId, ...next[1]);
+            } else {
+                await vscode.commands.executeCommand(commandId);
+            }
+            // here need calculate command count
+
+            this.commandIdToOverloadHandlerMap.set(
+                commandId,
+                vscode.commands.registerCommand(commandId, async (args) => commandHandler(commandId, args))
+            );
+        });
+    }
+
+    // Пока убирать не стал, потенциально здесь может что то хендлиться, что не хендлиться коммандами
+    private listenPublicVscodeApi() {
+        vscode.workspace.onDidCreateFiles((event) => {
+            // Potential keybindigs:
+            // welcome.showNewFileEntries
+            // workbench.action.files.newUntitledFile
+            // vscode.window.showInformationMessage('File created!');
+        });
+
+        vscode.workspace.onDidRenameFiles((event) => {
+            // Potential keybindigs:
+            // renameFile
+            // vscode.window.showInformationMessage('File renamed!');
+        });
+
+        vscode.window.onDidChangeActiveTextEditor((event) => {
+            // Potential keybindigs:
+            // many way to open from shortcuts:
+            // workbench.action.openEditorAtIndex1, workbench.action.openEditorAtIndex2...
+            // workbench.action.nextEditor, workbench.action.previousEditor,
+            // workbench.action.quickOpen
+
+            // workbench.action.closeActiveEditor
+
+            // Детектить что ничего из этого не было нажато, и сказать юзеру что вот смотри сколько у нас способов
+            // Кнопка посмотреть все шорткаты где будет перечисленны все перечисленные
+
+
+            // Нужно быть аккруатным т.к. эта лямбда дергается и при onDidCreateFiles и при onDidRenameFiles и когда мы просто закрываем едитор
+            // vscode.window.showInformationMessage('onDidChangeActiveTextEditor');
+        });
+
+        vscode.workspace.onDidSaveTextDocument((event) => {
+            // Potential keybindigs:
+            // workbench.action.files.save, workbench.action.files.saveAs, workbench.action.files.saveWithoutFormatting
+            // vscode.window.showInformationMessage('Text document save!');
+        });
+
+        vscode.window.onDidChangeWindowState((event) => {
+            // vscode.window.showInformationMessage('onDidChangeWindowState');
+        });
+
+        vscode.window.onDidOpenTerminal((event) => {
+            //Potential keybindigs:
+            // workbench.action.terminal.openNativeConsole, workbench.action.terminal.new
+            // and other workbench.action.terminal.* group
+            // vscode.window.showInformationMessage('onDidOpenTerminal');
+        });
+
+        vscode.window.onDidCloseTerminal((event) => {
+            //Potential keybindigs:
+            // workbench.action.terminal.kill
+            // vscode.window.showInformationMessage('onDidCloseTerminal');
+        });
+    }
+
+}
