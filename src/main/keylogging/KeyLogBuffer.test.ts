e3ca10fd77c879aa0bc2c8d12a452458d210ef34
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-14 11:46:56 +0300

    buffer test size 1

--- src/main/keylogging/KeyLogBuffer.test.ts
+++ src/main/keylogging/KeyLogBuffer.test.ts
@@ -4,7 +4,12 @@ import { KeyLogBuffer } from "./KeyLogBuffer";
 describe("Key Log Buffer Test", () => {
     it("buffer size 1", () => {
         var buf = new KeyLogBuffer(1);
+        assert.equal(buf.wasPressed("A"), false);
         buf.keyPressed("A");
         assert.equal(buf.wasPressed("A"), true);
+        assert.equal(buf.wasPressed("B"), false);
+        buf.keyPressed("B");
+        assert.equal(buf.wasPressed("A"), false);
+        assert.equal(buf.wasPressed("B"), true);
     });
 });
--- src/main/keylogging/KeyLogBuffer.ts
+++ src/main/keylogging/KeyLogBuffer.ts
@@ -1,15 +1,17 @@
 export class KeyLogBuffer {
 
     readonly size: number;
+    private lastKey?: string;
 
     constructor(size: number) {
         this.size = size;
     }
 
     keyPressed(key: string) {
+        this.lastKey = key;
     }
 
     wasPressed(key: string): Boolean {
-        return true;
+        return this.lastKey === key;
     }
 }
