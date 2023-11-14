f2d0fd1b0839d0b29017f4b106b45bc1d7c1cd0b
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-14 12:58:15 +0300

    key from keycode

---
+++ src/main/keylogging/transform.test.ts
@@ -0,0 +1,14 @@
+import * as assert from 'assert';
+import * as transform from './transform';
+import * as iohook from 'uiohook-napi';
+
+describe("Key Transform Test", () => {
+
+    it("some keys from keycode", () => {
+        assert.equal(transform.keyFromKeycode(iohook.UiohookKey.F10), "f10");
+        assert.equal(transform.keyFromKeycode(iohook.UiohookKey.W), "w");
+        assert.equal(transform.keyFromKeycode(iohook.UiohookKey.Alt), "alt");
+        assert.equal(transform.keyFromKeycode(iohook.UiohookKey.Escape), "escape");
+    });
+
+});
