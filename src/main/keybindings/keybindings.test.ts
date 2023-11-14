7e9c27124d3c81a4a6f6bb56aa61ff920eb7e457
Slava0135 <40753025+Slava0135@users.noreply.github.com>
2023-11-14 15:33:02 +0300

    Get default keybindings (#4)
    * download keybindings from repo
    
    * move keybindings out of src
    
    * parse json
    
    * get keybindings from json
    
    * get default bindings (linux)
    
    * platform specific keybindings
    
    * rename function

---
+++ src/main/keybindings/keybindings.test.ts
@@ -0,0 +1,32 @@
+import * as assert from 'assert';
+import * as keybindings from './keybindings';
+
+describe("Keybindings Test", () => {
+
+    it("get linux default keybindings", () => {
+        let bindings = keybindings.loadDefault("linux");
+        assert.deepEqual(bindings.get("editor.action.insertCursorAbove"), ["ctrl+shift+up", "shift+alt+up"]);
+        assert.equal(countBindings(bindings), 858);
+    });
+
+    it("get macos default keybindings", () => {
+        let bindings = keybindings.loadDefault("macos");
+        assert.deepEqual(bindings.get("editor.action.insertCursorAbove"), ["alt+cmd+up"]);
+        assert.equal(countBindings(bindings), 925);
+    });
+
+    it("get windows default keybindings", () => {
+        let bindings = keybindings.loadDefault("windows");
+        assert.deepEqual(bindings.get("editor.action.insertCursorAbove"), ["ctrl+alt+up"]);
+        assert.equal(countBindings(bindings), 866);
+    });
+
+});
+
+function countBindings(bindings: Map<string, string[]>) {
+    let count = 0;
+    bindings.forEach((keystrokes: string[], _: string) => {
+        count += keystrokes.length;
+    });
+    return count;
+}
---
+++ src/main/keybindings/keybindings.ts
@@ -0,0 +1,18 @@
+import { readFileSync } from 'fs';
+import * as json from "json5";
+
+class Keybinding {
+    key!: string;
+    command!: string;
+}
+
+export function loadDefault(platform: string): Map<string, string[]> {
+    let document = json.parse<Keybinding[]>(readFileSync(`./default-keybindings/${platform}.keybindings.json`).toString());
+    let keybindings = new Map<string, string[]>();
+    for (let i in document) {
+        let keystrokes = keybindings.get(document[i].command) ?? new Array<string>();
+        keystrokes.push(document[i].key);
+        keybindings.set(document[i].command, keystrokes);
+    }
+    return keybindings;
+}
