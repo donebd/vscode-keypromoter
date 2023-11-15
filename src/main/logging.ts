fec53aecca0229180c8a5099500c47d90263460b
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-15 15:16:46 +0300

    this works

--- src/main/logging.ts
+++ src/main/logging.ts
@@ -1,9 +1,12 @@
 import { Writable } from 'stream';
 import * as winston from 'winston';
+import * as vscode from 'vscode';
+
+const outputChannel = vscode.window.createOutputChannel("Key Promoter");
 
 const outputStream = new Writable({
     write(chunk: any, encoding, callback) {
-        console.log(chunk.toString());
+        outputChannel.append(chunk.toString());
     }
 });
 
