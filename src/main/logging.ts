78e436b8aca147b5e1e97056a297cf27b1f3fbc7
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-15 20:31:42 +0300

    nicer logs

--- src/main/logging.ts
+++ src/main/logging.ts
@@ -7,10 +7,16 @@ const defaultTransport = new winston.transports.Console();
 
 export const logger: winston.Logger = winston.createLogger({
     level: level,
-    format: winston.format.simple(),
+    format: winston.format.combine (
+        winston.format.timestamp({
+            format: "HH:mm:ss",
+        }),
+        winston.format.colorize({ all: true }),
+        winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`),
+    ),
     transports: [
         defaultTransport
-    ]
+    ],
 });
 
 export interface Channel {
