4541d365df8e62983d86995a52bf588a30822841
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-15 12:20:13 +0300

    many keys test

--- src/main/keylogging/KeyLogger.test.ts
+++ src/main/keylogging/KeyLogger.test.ts
@@ -9,6 +9,27 @@ describe("Key Logger Test", () => {
         assert.equal(keyLogger.hasAnyKeybinding(["w"]), false);
         keyLogger.handleKeyDown(UiohookKey.W);
         assert.equal(keyLogger.hasAnyKeybinding(["w"]), true);
+        keyLogger.handleKeyUp(UiohookKey.W);
+        assert.equal(keyLogger.hasAnyKeybinding(["w"]), false);
+    });
+
+    it("many keys", () => {
+        let keyLogger = new KeyLogger();
+        assert.equal(keyLogger.hasAnyKeybinding(["w"]), false);
+        assert.equal(keyLogger.hasAnyKeybinding(["ctrl+w"]), false);
+        assert.equal(keyLogger.hasAnyKeybinding(["w+ctrl"]), false);
+        assert.equal(keyLogger.hasAnyKeybinding(["ctrl"]), false);
+        keyLogger.handleKeyDown(UiohookKey.Ctrl);
+        keyLogger.handleKeyDown(UiohookKey.W);
+        assert.equal(keyLogger.hasAnyKeybinding(["w"]), false);
+        assert.equal(keyLogger.hasAnyKeybinding(["ctrl+w"]), true);
+        assert.equal(keyLogger.hasAnyKeybinding(["w+ctrl"]), false);
+        assert.equal(keyLogger.hasAnyKeybinding(["ctrl"]), false);
+        keyLogger.handleKeyUp(UiohookKey.Ctrl);
+        assert.equal(keyLogger.hasAnyKeybinding(["w"]), true);
+        assert.equal(keyLogger.hasAnyKeybinding(["ctrl+w"]), false);
+        assert.equal(keyLogger.hasAnyKeybinding(["w+ctrl"]), false);
+        assert.equal(keyLogger.hasAnyKeybinding(["ctrl"]), false);
     });
 
 });
