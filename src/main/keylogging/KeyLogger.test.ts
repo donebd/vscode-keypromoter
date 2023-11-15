b30bfd42f8e1a3c8bc21ef2260e6e22df1d01a3e
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-15 12:17:18 +0300

    single key test

--- src/main/keylogging/KeyLogger.test.ts
+++ src/main/keylogging/KeyLogger.test.ts
@@ -1,6 +1,14 @@
 import * as assert from 'assert';
 import { KeyLogger } from "./KeyLogger";
+import { UiohookKey } from 'uiohook-napi';
 
 describe("Key Logger Test", () => {
+
+    it("single key", () => {
+        let keyLogger = new KeyLogger();
+        assert.equal(keyLogger.hasAnyKeybinding(["w"]), false);
+        keyLogger.handleKeyDown(UiohookKey.W);
+        assert.equal(keyLogger.hasAnyKeybinding(["w"]), true);
+    });
 
 });
