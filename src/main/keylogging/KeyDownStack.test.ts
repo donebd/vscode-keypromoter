7f4e5e1587ac48a4dcb377499c0de0ebf91699b0
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-15 11:30:39 +0300

    many keys down

--- src/main/keylogging/KeyDownStack.test.ts
+++ src/main/keylogging/KeyDownStack.test.ts
@@ -3,10 +3,25 @@ import { KeyDownStack } from "./KeyDownStack";
 
 describe("Key Down Stack Test", () => {
 
-    it("single key press", () => {
+    it("single key down", () => {
         let stack = new KeyDownStack();
         stack.keyDown("A");
         assert.equal(stack.hasKeystroke(["A"]), true);
+    });
+
+    it("many keys down", () => {
+        let stack = new KeyDownStack();
+        stack.keyDown("A");
+        stack.keyDown("B");
+        stack.keyDown("C");
+        assert.equal(stack.hasKeystroke(["A"]), false);
+        assert.equal(stack.hasKeystroke(["B"]), false);
+        assert.equal(stack.hasKeystroke(["C"]), false);
+        assert.equal(stack.hasKeystroke(["A", "B"]), false);
+        assert.equal(stack.hasKeystroke(["B", "C"]), false);
+        assert.equal(stack.hasKeystroke(["A", "C"]), false);
+        assert.equal(stack.hasKeystroke(["A", "B", "C"]), true);
+        assert.equal(stack.hasKeystroke(["B", "A", "C"]), false);
     });
 
 });
