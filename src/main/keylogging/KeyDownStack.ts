d3a3e367c0dadc93414e059a1bb6f8bebf82d301
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-11-15 11:17:07 +0300

    key down stack methods

---
+++ src/main/keylogging/KeyDownStack.ts
@@ -0,0 +1,25 @@
+export class KeyDownStack {
+
+    private stack: Array<string>;
+
+    public constructor() {
+        this.stack = new Array();
+    }
+
+    public keyDown(key: string) {
+
+    }
+
+    public keyUp(key: string) {
+
+    }
+
+    public hasKeystroke() {
+
+    }
+
+    public reset() {
+        
+    }
+
+}
